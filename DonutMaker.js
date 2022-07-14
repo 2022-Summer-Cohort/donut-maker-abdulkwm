e82e5fbb65814e730fe341fc578b5f36568afab6
abdulkwm <abdu0921@hotmail.com>
2022-07-14 12:40:44 -0400

    finished project

---
+++ DonutMaker.js
@@ -0,0 +1,62 @@
+export { DonutMaker };
+
+class DonutMaker {
+
+    constructor() {
+        this._donutCount = 0;
+        this._autoClickerCount = 0;
+        this._autoClickerCost = 100;
+        this._donutMultiplier = 0;
+        this._donutMultiplierCost = 10;
+    
+    }
+
+    get donutCount() {
+        return this._donutCount;
+    }
+
+    get autoClickerCount() {
+        return this._autoClickerCount;
+    }
+    get autoClickerCost() {
+        return this._autoClickerCost;
+    }
+
+    get donutMultiplier() {
+        return this._donutMultiplier;
+    }
+
+    get donutMultiplierCost() {
+        return this._donutMultiplierCost;
+    }
+
+    recordClick() {
+        this._donutCount += Math.pow(1.2, this._donutMultiplier);
+    }
+
+    addAutoClicker() {
+        if (this._donutCount >= this._autoClickerCost) {
+            this._donutCount -= this._autoClickerCost;
+            this._autoClickerCount++;
+            this._autoClickerCost = this._autoClickerCost * 1.1;
+
+            if (this._autoClickerCount <= 1) {
+                this.activateAutoClickers();
+            }
+        }
+    }
+
+    activateAutoClickers() {
+
+        setInterval(() => {
+            this._donutCount += this._autoClickerCount * Math.pow(1.2, this._donutMultiplier);
+        }, 1000);
+    }
+    addDonutMultiplier() {
+        if (this._donutCount >= this._donutMultiplierCost) {
+            this._donutCount -= this._donutMultiplierCost;
+            this._donutMultiplier++;
+            this._donutMultiplierCost = this._donutMultiplierCost * 1.1;
+        }
+    }
+}
---
+++ app.js
@@ -0,0 +1,88 @@
+import { DonutMaker } from "/DonutMaker.js";
+
+const donutMaker = new DonutMaker();
+
+const updateDonutCount = function() {
+        const donuts = document.querySelector('.donut_count');
+        setInterval(() => {
+            donuts.innerText = `${Math.round(donutMaker.donutCount * 100) / 100} Donuts! `;
+            checkAutoClickerButton();
+            checkDonutMultiplierButton();
+        }, 1);
+    }
+const clicks = document.querySelector('.add__donut');
+for (var b = 0; b < clicks.length; b++) {
+    clicks.src = "/image/donut.jpeg";
+}
+
+clicks.addEventListener('click', () => {
+    updateClickButtonDisplay();
+    donutMaker.recordClick();
+    updateDonutCount();
+
+})
+
+function updateClickButtonDisplay() {
+    const donutClicks = document.querySelector('.donut__clicks');
+    donutClicks.innerText = `Click for ${Math.round(multiplierValue() * 100) / 100} Donuts!`;
+}
+
+const updateAutoClickerCount = function() {
+    const autoClickerCount = document.querySelector('.auto_clicker_count');
+    autoClickerCount.innerText = donutMaker.autoClickerCount;
+}
+
+const autoClickers = document.querySelector('.purchase__auto_clickers');
+autoClickers.addEventListener('click', () => {
+    autoClickers.innerText = `Purchase Auto Clickers for ${Math.round(donutMaker.autoClickerCost * 100) / 100} Donuts!`
+    donutMaker.addAutoClicker();
+    updateAutoClickerCount();
+    updateDonutCount();
+})
+
+function checkAutoClickerButton() {
+    if (donutMaker.donutCount >= donutMaker.autoClickerCost) {
+        autoClickers.disabled = false;
+    } else {
+        autoClickers.disabled = true;
+    }
+}
+
+const updateMultiplierCount = function() {
+    const multiplierCount = document.querySelector('.donut_multiplier_count');
+    multiplierCount.innerText = donutMaker.donutMultiplier;
+}
+
+const donutMultiplier = document.querySelector('.purchase_donut_mulitipliers');
+donutMultiplier.addEventListener('click', () => {
+    donutMultiplier.innerText = `Purchase Multiplier for ${Math.round(donutMaker.donutMultiplierCost * 100) / 100} Donuts!`
+    donutMaker.addDonutMultiplier();
+    updateMultiplierCount();
+    updateDonutCount();
+    updateClickButtonDisplay();
+})
+
+function checkDonutMultiplierButton() {
+    if (donutMaker.donutCount >= donutMaker.donutMultiplierCost) {
+        donutMultiplier.disabled = false;
+    } else {
+        donutMultiplier.disabled = true;
+    }
+}
+
+function multiplierValue() {
+    return Math.pow(1.2, donutMaker.donutMultiplier);
+}
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("modalBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
